034a2f338634a2fb09a2bf460bce6dbd98f63258
Louis Sullivan <sullivanlouis0@gmail.com>
2023-01-15 00:06:39 +0000

    added css for home page, login and sidebar

--- skillcloud-frontend/src/webpages/Home.js
+++ skillcloud-frontend/src/webpages/Home.js
@@ -1,7 +1,8 @@
-import Sidebar from "../components/Sidebar";
-import ProjectTile from "../components/Project/ProjectTile";
+import React, { useEffect, useState } from 'react'
+import { useNavigate, Link } from "react-router-dom";
 
-import { useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Pagination from "../components/Project/Pagination";
 
 import "../index.css";
 
@@ -11,19 +12,54 @@ const Home = () => {
         let path = `/createproject`; 
         navigate(path);
     }
+
+    const [tileData, setTileData] = useState([]); 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [tilesPerPage] = useState(12);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const resp = await fetch('http://127.0.0.1:5000/home')
+        const data = await resp.json();
+        setTileData(data.result);
+      };
+      fetchData()
+    }, []);
+
+    const indexOfData= currentPage * tilesPerPage;
+    const indexOfFData = indexOfData - tilesPerPage;
+    const curData = tileData.slice(indexOfFData, indexOfData);
+    const nPages = Math.ceil(tileData.length / tilesPerPage)
+
     return (
         <div className="app">
             <Sidebar/>
             <div className="page">
-                <div className="page-heading">
+                <div className="home-page-heading">
                     <h1>Home</h1>
-                    <button type="button" className="but-pos" onClick={handleClick}>
+                    <button type="button" onClick={handleClick}>
                         Create Project
                     </button>
                 </div>
-                <div className="page-content">
-                    <ProjectTile/>
-                </div>
+                <div className="home-page">
+                    <div className='card-wrapper'>
+                        {curData.map((tileData, k) => (
+                            <div className='card' key={k}>
+                                <Link to={`/project/${tileData.id}`} style={{textDecoration: "none"}}>
+                                        <div className='card-header'>{tileData.title}</div>
+                                            <div className='card-body'>
+                                                {tileData.summary}...
+                                            </div>
+                                    {tileData.state === 'Open' ? <div className="card-footer" style={{color: "green"}}>State: {tileData.state}</div> : <div className="card-footer" style={{color: "red"}}>State: {tileData.state}</div>}
+                                </Link>
+                            </div>
+                            ))}
+                        </div>
+                    </div>
+                <Pagination
+                nPages={nPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}/>
             </div>
         </div>
     );
